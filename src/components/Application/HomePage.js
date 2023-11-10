0cc1a6cc8eb61cbe3a5718dfb97e190671bcdd07
Andrey <94688521+Andrew300189@users.noreply.github.com>
2023-11-10 17:24:49 +0200

    Update HomePage.js

--- src/components/Application/HomePage.js
+++ src/components/Application/HomePage.js
@@ -1,19 +1,10 @@
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 const HomePage = () => (
   <div>
     <h1>Welcome to the Contacts App</h1>
     <p>Please login or register to access your contacts.</p>
-    <div>
-      <NavLink to="/login">
-        <button>Login</button>
-      </NavLink>
-      <NavLink to="/register">
-        <button>Register</button>
-      </NavLink>
-    </div>
   </div>
 );
 
