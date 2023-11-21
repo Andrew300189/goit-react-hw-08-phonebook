c593c9908f770b83d47b66943e79d2d4e992531f
Andrey <94688521+Andrew300189@users.noreply.github.com>
2023-11-21 22:30:27 +0200

    Changes

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,64 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { loginUser } from '../../redux/userSlice';
-import styles from './Login.module.css';
+
+import React from 'react';
+import LoginForm from './LoginForm';
 
 const Login = () => {
-  const history = useNavigate();
-  const dispatch = useDispatch();
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = async (e) => {
-    e.preventDefault();
-
-    try {
-      if (!email || !password) {
-        console.error('Email and password are required');
-        return;
-      }
-
-      const userData = {
-        email,
-        password,
-      };
-
-      const action = await dispatch(loginUser(userData));
-
-      if (loginUser.fulfilled.match(action)) {
-        const response = action.payload;
-
-        console.log(response);
-
-        history('/contacts'); 
-      } else {
-        console.error('Login failed. Unexpected response status:', action.error.message);
-      }
-    } catch (error) {
-      console.error('Login failed:', error.message);
-    }
-  };
-
   return (
-    <div className={styles.container}>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin} className={styles.form}>
-        <label className={styles.formLabel}>
-          Email
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className={styles.formInput} />
-        </label>
-        <br />
-        <label className={styles.formLabel}>
-          Password
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className={styles.formInput} />
-        </label>
-        <br />
-        <button type="submit" className={styles.formButton}>
-          Login
-        </button>
-      </form>
+    <div>
+      <LoginForm />
     </div>
   );
 };
---
+++ src/components/Login/LoginForm.js
@@ -0,0 +1,76 @@
+// LoginForm.js
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginUser } from 'redux/userSlice';
+import { useNavigate } from 'react-router-dom';
+import styles from './Login.module.css';
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+  const history = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      if (!email || !password) {
+        console.error('Email and password are required');
+        return;
+      }
+
+      const userData = {
+        email,
+        password,
+      };
+
+      const action = await dispatch(loginUser(userData));
+
+      if (loginUser.fulfilled.match(action)) {
+        const response = action.payload;
+
+        console.log(response);
+
+        history.push('/contacts');
+      } else {
+        console.error('Login failed. Unexpected response status:', action.error.message);
+      }
+    } catch (error) {
+      console.error('Login failed:', error.message);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <label className={styles.formLabel}>
+          Email
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={styles.formInput}
+          />
+        </label>
+        <br />
+        <label className={styles.formLabel}>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={styles.formInput}
+          />
+        </label>
+        <br />
+        <button type="submit" className={styles.formButton}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
